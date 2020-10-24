dba75557f10de9aaa1811c90899c583cee2aa86d
amal <amal@MacBook-de-zaki.local>
2020-10-24 01:17:20 +0100

    yarebi

--- app/config/config.php
+++ app/config/config.php
@@ -1,8 +1,8 @@
 <?php
   // DB Params
-  define('DB_HOST', 'mysql');
+  define('DB_HOST', 'localhost');
   define('DB_USER', 'root');
-  define('DB_PASS', 'tiger');
+  define('DB_PASS', '');
   define('DB_NAME', 'camagru');
 
   // App Root
