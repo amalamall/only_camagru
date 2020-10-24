5a7ab96faac481e5f41eacaecb583459a19f1eb3
amal <amal@MacBook-de-zaki.local>
2020-10-24 16:57:12 +0100

    ya rebi

--- app/config/Core.php
+++ app/config/Core.php
@@ -15,12 +15,15 @@
       $url = $this->getUrl();
 
       // Look in controllers for first value
-      if(file_exists($_SERVER['DOCUMENT_ROOT'].'/camagru/app/controllers/' . ucwords($url[0]). '.php')){
-        // If exists, set as controller
-        $this->currentController = ucwords($url[0]);
-        // Unset 0 Index
-        unset($url[0]);
+      if(isset($url[0])){
+                  if(file_exists($_SERVER['DOCUMENT_ROOT'].'/camagru/app/controllers/' . ucwords($url[0]). '.php')){
+            // If exists, set as controller
+            $this->currentController = ucwords($url[0]);
+            // Unset 0 Index
+            unset($url[0]);
+          }
       }
+
 
       // Require the controller
       require_once $_SERVER['DOCUMENT_ROOT'].'/camagru/app/controllers/'. $this->currentController . '.php';
