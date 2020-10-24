fafc1bcd6133ac519e54f1e047aea23bcb4322a0
amal <amal@MacBook-de-zaki.local>
2020-10-24 17:07:19 +0100

    ya rebi

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,14 +1,14 @@
 <?php  
   
   // Load Config
-  require_once 'config/config.php';
+  require_once $_SERVER['DOCUMENT_ROOT'].'/camagru/app/config/config.php';
   //load Helpers
-  require_once 'helpers/url_helper.php';
-  require_once 'helpers/session_helper.php';
+  require_once $_SERVER['DOCUMENT_ROOT'].'/camagru/app/helpers/url_helper.php';
+  require_once $_SERVER['DOCUMENT_ROOT'].'/camagru/app/helpers/session_helper.php';
   // Autoload Core Libraries
-  require_once 'config/Controller.php';
-  require_once 'config/Core.php';
-  require_once 'config/database.php';
+  require_once $_SERVER['DOCUMENT_ROOT'].'/camagru/app/config/Controller.php';
+  require_once $_SERVER['DOCUMENT_ROOT'].'/camagru/app/config/Core.php';
+  require_once $_SERVER['DOCUMENT_ROOT'].'/camagru/app/config/database.php';
 
   // spl_autoload_register(function($className){
   //   require_once 'config/' . $className . '.php';
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  require_once 'app/bootstrap.php';
-  require_once 'app/config/setup.php';
+  require_once $_SERVER['DOCUMENT_ROOT'].'/camagru/app/bootstrap.php';
+  require_once $_SERVER['DOCUMENT_ROOT'].'/camagru/app/config/setup.php';
   // Init Core Library
   $init = new Core;
