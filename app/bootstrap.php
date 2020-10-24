2fc1393e714de760ce03c06e2e2c9aef8eb01bb3
amal <amal@MacBook-de-zaki.local>
2020-10-24 03:56:57 +0100

    yarebi

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,13 +1,17 @@
 <?php  
   
   // Load Config
-  require_once './config/config.php';
+  require_once 'config/config.php';
   //load Helpers
-  require_once './helpers/url_helper.php';
-  require_once './helpers/session_helper.php';
+  require_once 'helpers/url_helper.php';
+  require_once 'helpers/session_helper.php';
   // Autoload Core Libraries
-  spl_autoload_register(function($className){
-    require_once './config/' . $className . '.php';
-  });
+  require_once 'config/Controller.php';
+  require_once 'config/Core.php';
+  require_once 'config/database.php';
+
+  // spl_autoload_register(function($className){
+  //   require_once 'config/' . $className . '.php';
+  // });
 
   
